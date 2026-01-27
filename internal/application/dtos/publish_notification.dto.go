1a9914df52f89d57e9aa55cd60f14171ce473397
DiegoGPAT <diego.gutierrez@apuestatotal.com>
2026-01-27 09:32:40 -0500

    wip: publish notification

--- internal/application/dtos/publish_notification.dto.go
+++ internal/application/dtos/publish_notification.dto.go
@@ -1,4 +1,10 @@
 package dtos
 
 type PublishNotificationRequest struct {
+	TargetArn  string            `json:"targetArn,omitempty"` // endpoint
+	TopicArn   string            `json:"topicArn,omitempty"`  // topic
+	Title      string            `json:"title"`
+	Body       string            `json:"body"`
+	Data       map[string]string `json:"data,omitempty"`
+	Attributes map[string]string `json:"attributes,omitempty"` // filtros SNS
 }
--- internal/application/usecases/publish_notification.usecase.go
+++ internal/application/usecases/publish_notification.usecase.go
@@ -2,23 +2,67 @@ package usecases
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"lmbd-digital-push-notifications/internal/application/contracts/services"
 	"lmbd-digital-push-notifications/internal/application/dtos"
+	"log/slog"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns/types"
 )
 
 type PublishNotificationUseCase struct {
 	snsService services.ISnsService
+	logger     *slog.Logger
 }
 
-func NewPublishNotificationUseCase(snsService services.ISnsService) *PublishNotificationUseCase {
+func NewPublishNotificationUseCase(snsService services.ISnsService, logger *slog.Logger) *PublishNotificationUseCase {
 	return &PublishNotificationUseCase{
 		snsService: snsService,
+		logger:     logger,
 	}
 }
 
-func (uc *PublishNotificationUseCase) Execute(ctx context.Context, data dtos.PublishNotificationRequest) error {
-	// Lógica para enviar la notificación
-	fmt.Print(data)
+func (uc *PublishNotificationUseCase) Execute(ctx context.Context, rq dtos.PublishNotificationRequest) error {
+	uc.logger.Info("PublishNotificationUseCase started:",
+		"TopicArn", rq.TopicArn,
+		"TargetArn", rq.TargetArn,
+	)
+
+	if rq.TargetArn == "" && rq.TopicArn == "" {
+		return errors.New("targetArn or topicArn is required")
+	}
+
+	dataBytes, _ := json.Marshal(rq.Data)
+
+	payload := map[string]string{
+		"default": rq.Body,
+		"GCM": fmt.Sprintf(`{
+		"notification": {
+			"title": "%s",
+			"body": "%s"
+		},
+		"data": %s
+	}`, rq.Title, rq.Body, string(dataBytes)),
+	}
+
+	attrs := services.SnsMessageAttributes{}
+
+	for k, v := range rq.Attributes {
+		attrs[k] = types.MessageAttributeValue{
+			DataType:    aws.String("String"),
+			StringValue: aws.String(v),
+		}
+	}
+
+	publishOptions := services.SnsPublishOptions{
+		Attributes: attrs,
+		Subject:    rq.Title,
+	}
+
+	uc.snsService.Publish(ctx, &rq.TopicArn, &rq.TargetArn, payload, &publishOptions)
+
 	return nil
 }
